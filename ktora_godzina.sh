277d3a140096038593663015d5cf572158e3f58f
s8@pakiso <s8@pakiso>
2026-04-28 15:06:52 +0000

    v1.2

--- ktora_godzina.sh
+++ ktora_godzina.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 #moj komentarz 
-
-h_end=18
+echo "Podaj o której kończysz prace"
+read h_end m_end
 
 h_now=$(($(date +%H)+2))
 m_now=$(date +%M)
 
-h_left=$((h_end-h_now-1))
-m_left=$((60-$m_now))
+if [ $m_end -lt $m_now ]
+then
+    h_end=$((h_end-1))
+    m_en=$((m_end+60))
+fi
+
+h_left=$((h_end-h_now))
+m_left=$((m_end-m_now))
 
 echo "Jest godzina: $h_now:$m_now !"
 echo "Do końca szychty zostało: $h_left h $m_left min"
