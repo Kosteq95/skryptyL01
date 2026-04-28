2b2d91bc75a16016aaf134494e7007f452f9cd81
s8@pakiso <s8@pakiso>
2026-04-28 14:53:29 +0000

    ver1

--- ktora_godzina.sh
+++ ktora_godzina.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 #moj komentarz 
 
-godzina="$(($(date +%H)+2)):$(date +%M)"
+h_end=18
 
-echo "Jest godzina: $godzina !"
-echo "test zdalnego repo"
-echo "test2"
+h_now=$(($(date +%H)+2))
+m_now=$(date +%M)
+
+h_left=$((h_end-h_now-1))
+m_left=$((60-$m_now))
+
+echo "Jest godzina: $h_now:$m_now !"
+echo "Do końca szychty zostało: x:y"
