cf6c97344b88d53dab8eef57f80206174c147ff8
s8@pakiso <s8@pakiso>
2026-05-05 14:39:35 +0000

    podstawka

--- dodaj_pojazd.sh
+++ dodaj_pojazd.sh
@@ -18,3 +18,23 @@
 # Data ostatniego przegladu: DD.MM.YYYY 
 # Przebieg na ostatnim przeglądzie: ...... km 
 # Ostania aktualizacja odbyła się DD.MM.YYYY HH:MM:SS przez "nazwa użytkownika systemu"
+
+
+
+
+read -p "Podaj markę samochodu: " marka
+read -p "Podaj model samochodu: " model
+read -p "Podaj rocznik samochodu: " rocznik
+read -p "Podaj numer rejestracyjny samochodu: " nr_rej
+read -p "Podaj datę ostatniego przeglądu i przebieg samochodu: (DD:MM:YYYY) " data_przegladu przebieg
+
+
+echo "-------------- CAR PASSPORT ----------------- 
+Marka: $marka
+Model: $model
+Rocznik: $rocznik 
+Numer rejestracyjny: $nr_rej
+Data ostatniego przegladu: $data_przegladu
+Przebieg na ostatnim przeglądzie: $przebieg km 
+Ostatnia aktualizacja odbyła się $(date +%d.%m.%Y\ %X) przez $USER > $nr_rej.pc
+
