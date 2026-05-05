2bc68745c2bff37c12e73f20681e5b2a7670e228
s8@pakiso <s8@pakiso>
2026-05-05 15:21:54 +0000

    v0.3a

--- dodaj_pojazd.sh
+++ dodaj_pojazd.sh
@@ -18,3 +18,59 @@
 # Data ostatniego przegladu: DD.MM.YYYY 
 # Przebieg na ostatnim przeglądzie: ...... km 
 # Ostania aktualizacja odbyła się DD.MM.YYYY HH:MM:SS przez "nazwa użytkownika systemu"
+
+function pobierz_dane
+{
+    licznik=0
+
+    while [ -z "$dana" ]
+    do
+        read -p "Podaj $1 samochodu" dana
+        licznik=$((licznik+1))
+
+        if [ $licznik -gt 3 ]
+        then
+            echo "Nie podałeś wymaganych danych" 1>&2
+            kill -term $$ 
+        fi
+    done
+    
+    echo $dana
+}
+
+
+
+typ="osobowe"
+
+if [ "$1" = "-c" ] 
+then
+ typ="ciezarowe"
+elif [ -n "$1" ] && [ "$1" != "-o" ]
+then
+    echo "Nie poprawny argument typu pojazdu" 1>&2
+    exit 1
+fi
+
+#read -p "Podaj markę samochodu: " marka
+marka=$(pobierz_dane "markę")
+read -p "Podaj model samochodu: " model
+read -p "Podaj rocznik samochodu: " rocznik
+read -p "Podaj numer rejestracyjny samochodu: " nr_rej
+read -p "Podaj datę ostatniego przeglądu i przebieg samochodu: (DD:MM:YYYY) " data_przegladu przebieg
+
+dir_path="$HOME/Baza_Pojazdow/$typ/$marka"
+
+if ! [ -d $dir_path ]
+then
+  mkdir -p $dir_path
+fi
+
+echo "-------------- CAR PASSPORT ----------------- 
+Marka: $marka
+Model: $model
+Rocznik: $rocznik 
+Numer rejestracyjny: $nr_rej
+Data ostatniego przegladu: $data_przegladu
+Przebieg na ostatnim przeglądzie: $przebieg km 
+Ostatnia aktualizacja odbyła się $(date +%d.%m.%Y\ %X) przez $USER" > $dir_path/$nr_rej.pc
+
